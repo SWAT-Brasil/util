b1026ceee6dbb530be64c56bac6e632de1e33cf4
jairo <jairo.rotava@gmail.com>
2020-10-29 11:02:32 -0300

    Ajuste do examplo

--- pcpswat/test/test_pcpswat.py
+++ pcpswat/test/test_pcpswat.py
@@ -8,8 +8,8 @@ EXEC_PATH = os.path.realpath(os.path.join(here, '../pcpswat.py'))
 
 
 def test_get_netcdf_to_db():
-    cmd = ['python', EXEC_PATH, os.path.realpath('./exemplo_1/pcp/pcp.txt'), os.path.realpath('./out/'),
-           '-interpolate', os.path.realpath("./exemplo_1/pcp/pcp_to_interpolate.txt"), "-method", 'nearest']
+    cmd = ['python', EXEC_PATH, os.path.realpath('./example_1/pcp/pcp.txt'), os.path.realpath('./out/'),
+           '-interpolate', os.path.realpath("./example_1/pcp/pcp_to_interpolate.txt"), "-method", 'nearest']
     print('\nComando teste -> ' + ' '.join(cmd))
     output = subprocess.run(cmd)
     print(output.stdout)
